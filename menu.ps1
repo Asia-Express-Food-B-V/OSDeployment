4b49d34618e2728bf46045989d56429078818b57
pmekes <p.mekes@asiaexpressfood.nl>
2026-01-21 13:57:05 +0100

    Update menu.ps1
    Add option to install only Windows through OSDCloud GUI

--- menu.ps1
+++ menu.ps1
@@ -68,6 +68,9 @@ $listBox.Width  = 300
 [void] $listBox.Items.Add('-------- Brouwer Horeca --------')
 [void] $listBox.Items.Add('Personal-BRH')
 [void] $listBox.Items.Add('Shared-BRH')
+[void] $listBox.Items.Add(' ')
+[void] $listBox.Items.Add('-------- Windows Only --------')
+[void] $listBox.Items.Add('Windows')
 $form.Controls.Add($listBox)
 
 $form.Topmost = $true
