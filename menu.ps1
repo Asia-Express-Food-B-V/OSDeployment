aa1bef7c50d692c67666ea7600f583006d0ecacf
pmekes <p.mekes@asiaexpressfood.nl>
2026-06-17 10:31:16 +0200

    Update menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -1,4 +1,3 @@
-<#
 #================================================
 # Window Functions
 # Minimize Command and PowerShell Windows
@@ -27,11 +26,11 @@ Add-Type -AssemblyName System.Drawing
 
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Computer Profile'
-$form.Size = New-Object System.Drawing.Size(500,400)
+$form.Size = New-Object System.Drawing.Size(400,475)
 $form.StartPosition = 'CenterScreen'
 
 $okButton = New-Object System.Windows.Forms.Button
-$okButton.Location = New-Object System.Drawing.Point(150,300)
+$okButton.Location = New-Object System.Drawing.Point(150,370)
 $okButton.Size = New-Object System.Drawing.Size(75,23)
 $okButton.Text = 'OK'
 $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
@@ -39,7 +38,7 @@ $form.AcceptButton = $okButton
 $form.Controls.Add($okButton)
 
 $cancelButton = New-Object System.Windows.Forms.Button
-$cancelButton.Location = New-Object System.Drawing.Point(225,300)
+$cancelButton.Location = New-Object System.Drawing.Point(225,370)
 $cancelButton.Size = New-Object System.Drawing.Size(75,23)
 $cancelButton.Text = 'Cancel'
 $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
@@ -55,96 +54,7 @@ $form.Controls.Add($label)
 $listBox = New-Object System.Windows.Forms.ListBox
 $listBox.Location = New-Object System.Drawing.Point(10,40)
 $listBox.Size = New-Object System.Drawing.Size(260,20)
-$listBox.Height = 300
-$listBox.Width  = 300
-[void] $listBox.Items.Add('-------- Asia Express Food --------')
-[void] $listBox.Items.Add('Personal')
-[void] $listBox.Items.Add('Shared-AEF')
-[void] $listBox.Items.Add('Kiosk')
-[void] $listBox.Items.Add('Passwordless')
-[void] $listBox.Items.Add(' ')
-[void] $listBox.Items.Add('-------- Schiava --------')
-[void] $listBox.Items.Add('Personal-SCH')
-[void] $listBox.Items.Add('Shared-SCH')
-[void] $listBox.Items.Add(' ')
-[void] $listBox.Items.Add('-------- Brouwer Horeca --------')
-[void] $listBox.Items.Add('Personal-BRH')
-[void] $listBox.Items.Add('Shared-BRH')
-[void] $listBox.Items.Add(' ')
-[void] $listBox.Items.Add('-------- Windows Only --------')
-[void] $listBox.Items.Add('Windows')
-$form.Controls.Add($listBox)
-
-$form.Topmost = $true
-
-$result = $form.ShowDialog()
-
-if ($result -eq [System.Windows.Forms.DialogResult]::OK)
-{
-    $x = $listBox.SelectedItem
-    $GroupTag = $x
-}
-Show-PowershellWindow
-#>
-
-
-#================================================
-# Window Functions
-# Minimize Command and PowerShell Windows
-#================================================
-$Script:showWindowAsync = Add-Type -MemberDefinition @"
-[DllImport("user32.dll")]
-public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
-"@ -Name "Win32ShowWindowAsync" -Namespace Win32Functions -PassThru
-function Hide-CmdWindow() {
-    $CMDProcess = Get-Process -Name cmd -ErrorAction Ignore
-    foreach ($Item in $CMDProcess) {
-        $null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $Item.id).MainWindowHandle, 2)
-    }
-}
-function Hide-PowershellWindow() {
-    $null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 2)
-}
-function Show-PowershellWindow() {
-    $null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 10)
-}
-Hide-CmdWindow
-Hide-PowershellWindow
-
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-
-$form = New-Object System.Windows.Forms.Form
-$form.Text = 'Computer Profile'
-$form.Size = New-Object System.Drawing.Size(400,420)
-$form.StartPosition = 'CenterScreen'
-
-$okButton = New-Object System.Windows.Forms.Button
-$okButton.Location = New-Object System.Drawing.Point(150,350)
-$okButton.Size = New-Object System.Drawing.Size(75,23)
-$okButton.Text = 'OK'
-$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
-$form.AcceptButton = $okButton
-$form.Controls.Add($okButton)
-
-$cancelButton = New-Object System.Windows.Forms.Button
-$cancelButton.Location = New-Object System.Drawing.Point(225,350)
-$cancelButton.Size = New-Object System.Drawing.Size(75,23)
-$cancelButton.Text = 'Cancel'
-$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
-$form.CancelButton = $cancelButton
-$form.Controls.Add($cancelButton)
-
-$label = New-Object System.Windows.Forms.Label
-$label.Location = New-Object System.Drawing.Point(10,20)
-$label.Size = New-Object System.Drawing.Size(280,20)
-$label.Text = 'Please select a profile:'
-$form.Controls.Add($label)
-
-$listBox = New-Object System.Windows.Forms.ListBox
-$listBox.Location = New-Object System.Drawing.Point(10,40)
-$listBox.Size = New-Object System.Drawing.Size(260,20)
-$listBox.Height = 300
+$listBox.Height = 320
 $listBox.Width  = 300
 [void] $listBox.Items.Add('-------- Asia Express Food --------')
 [void] $listBox.Items.Add('Personal')
